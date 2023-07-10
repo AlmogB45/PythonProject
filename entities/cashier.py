b3a507898899489992e1f0bbc3ddbf1a0fc45c34
Almog Ben Yamin <almoggx@gmail.com>
2023-07-10 19:36:26 +0300

    Fixed up the Cashier and Cashier_Menu so the methods will be in the Cashier entity and not in the menu
    Added extra identifications for a cashier who wish to sell a product to a costumer (i.e: Register number, ID, Name, Age, Phone number etc)

--- entities/cashier.py
+++ entities/cashier.py
@@ -1,21 +1,54 @@
 from data.person import Person
+from entities.Product import Product
 from entities.client import Client
 from entities.employee import Employee
 
 
 class Cashier(Employee):
+
     def __init__(self, register_number: int, employee_number: int, ID: int, name: str, age: int, phone_number: str):
         super().__init__(employee_number, ID, name, age, phone_number)
         self.register_number = register_number
+        self.products = self.read_product_from_file()
 
-    def purchase_product(self, client: Client, product):
-        client.purchased_products.append(product)
+    def sell_product_to_customer(self):
+        product_name = input("Enter the product name: ")
+        customer_name = input("Enter the customer name: ")
+        self.purchase_product(product_name, customer_name)
 
-    def __str__(self):
-        print(f"register number: {self.register_number}")
-        super().__str__()
+    def purchase_product(self, product_name, customer_name):
 
-    def __eq__(self, other):
-        if isinstance(other, Cashier):
-            return self.employee_number == other.employee_number
-        return False
+        # Check if the product exists in the file.
+        product = None
+        for prod in self.products:
+            if prod.name == product_name:
+                product = prod
+                break
+
+        if product is None:
+            print("Product not found.")
+            return
+
+        # Print a purchase successful message.
+        print(f"Purchase successful for {customer_name}.")
+
+    def read_product_from_file(self):
+        products = []
+
+        try:
+            with open("C:\\Users\\Almog-Laptop\\OneDrive\\Desktop\\FinalSuper\\data\\products.txt", 'r') as file:
+                for line in file:
+                    data = line.strip().split(",")
+
+                    if len(data) == 3:
+                        name = data[0]
+                        type = data[1]
+                        price = float(data[2])  # Convert the price to float
+
+                        prod = Product(name, type, price)
+                        products.append(prod)
+
+        except IOError as e:
+            print("Error reading file:", e)
+
+        return products
--- menu/cashier_menu.py
+++ menu/cashier_menu.py
@@ -1,13 +1,10 @@
 import sys
-from entities.Product import Product
+from entities import cashier
 
+from entities.cashier import Cashier
 
 
 class CashierMenu:
-
-    def __init__(self):
-        self.products = self.read_product_from_file()
-
     def display_menu(self):
         while True:
             print("-------- Cashier Menu --------")
@@ -17,7 +14,19 @@ class CashierMenu:
 
             choice = input("Enter your choice: ")
             if choice == "1":
-                self.sell_product_to_customer()
+                print("Please identify yourself and choose the register you wish to operate (1-10)")
+                register_number = int(input("Enter the register number: "))
+                if register_number > 10:
+                    print("Invalid credentials, returning to home page")
+                    break
+                employee_number = int(input("Enter the employee number: "))
+                ID = int(input("Enter the ID: "))
+                name = input("Enter the name: ")
+                age = int(input("Enter the age: "))
+                phone_number = input("Enter the phone number: ")
+                cashier.Cashier(register_number, employee_number, ID, name, age,
+                                phone_number).sell_product_to_customer()
+
             elif choice == "2":
                 print("\n")
                 print("*** thank you and see you soon at the super ***")
@@ -26,55 +35,6 @@ class CashierMenu:
                 break
             else:
                 print("Invalid choice. Please try again.")
-
-    def sell_product_to_customer(self):
-        product_name = input("Enter the product name: ")
-        customer_name = input("Enter the customer name: ")
-        self.purchase_product(product_name, customer_name)
-
-    def purchase_product(self, product_name, customer_name):
-        """Purchases a product from the product.txt file.
-
-        Args:
-          product_name: The name of the product to purchase.
-          customer_name: The name of the customer purchasing the product.
-        """
-
-        # Check if the product exists in the file.
-        product = None
-        for prod in self.products:
-            if prod.name == product_name:
-                product = prod
-                break
-
-        if product is None:
-            print("Product not found.")
-            return
-
-        # Print a purchase successful message.
-        print(f"Purchase successful for {customer_name}.")
-
-    def read_product_from_file(self):
-        products = []
-
-        try:
-            with open("C:\\Users\\Almog-Laptop\\OneDrive\\Desktop\\FinalSuper\\data\\products.txt", 'r') as file:
-                for line in file:
-                    data = line.strip().split(",")
-
-                    if len(data) == 3:
-                        name = data[0]
-                        type = data[1]
-                        price = float(data[2])  # Convert the price to float
-
-                        prod = Product(name, type, price)
-                        products.append(prod)
-
-        except IOError as e:
-            print("Error reading file:", e)
-
-        return products
-
 
 if __name__ == "__main__":
     cashier_menu = CashierMenu()
